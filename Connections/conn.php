fbb6732292f384c43e4067adead99bff640f878a
gerinsp <denapratama7@gmail.com>
2024-01-26 00:52:49 +0700

    ganti server

--- Connections/conn.php
+++ Connections/conn.php
@@ -3,10 +3,10 @@
 # Type="MYSQL"
 # HTTP="true"
 
-$hostname_conn = "localhost";
-$database_conn = "wisata_jogja";
-$username_conn = "root";
-$password_conn = "";
+$hostname_conn = "sql310.infinityfree.com";
+$database_conn = "if0_35859875_wisata_jogja";
+$username_conn = "if0_35859875";
+$password_conn = "y3I3pwNkdy";
 
 $conn = mysql_pconnect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR);
 
